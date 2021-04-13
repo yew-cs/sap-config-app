6858be1fe002ffeebd22aa05f8494eb7988e9c98
Chuo Shing Yew <chuoshing.yew@rizing.com>
2021-04-13 15:09:02 +0800

    Bo app deleted

--- app/businessobjects/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
-    'use strict';
-
-    return AppComponent.extend("businessobjects.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
